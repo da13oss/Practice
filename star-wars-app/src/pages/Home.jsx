9e8ce6fe4928bd7e0128d5e124a190db374026f2
Benjamin Pettis <48529614+da13oss@users.noreply.github.com>
2024-10-03 08:17:48 -0500

    Star_Wars_API
    Bringing in the star wars API and access the information.

---
+++ star-wars-app/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchForm from '../components/SearchForm';
+
+const Home = () => (
+    <div>
+        <h1>Welcome to Luke APIWalker</h1>
+        <SearchForm />
+    </div>
+);
+
+export default Home;
