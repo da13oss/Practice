76edf12b8b9bb4288b60e18c2c7b3082f7807496
Benjamin Pettis <48529614+da13oss@users.noreply.github.com>
2024-10-05 07:06:02 -0500

    Practice for Javascript - MERN
    Here is another practice project

---
+++ single_state_form/form copy/src/components/Form.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import './Form.css';
+
+const Form = () => {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    return (
+        <div>
+            <form>
+                <div>
+                    <label>First Name:</label>
+                    <input
+                        type="text"
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label>Last Name:</label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label>Confirm Password:</label>
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                    />
+                </div>
+            </form>
+            <div className="form-data">
+                <h3>Your Form Data:</h3>
+                <p>First Name: {formData.firstName}</p>
+                <p>Last Name: {formData.lastName}</p>
+                <p>Email: {formData.email}</p>
+                <p>Password: {formData.password}</p>
+                <p>Confirm Password: {formData.confirmPassword}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Form;
